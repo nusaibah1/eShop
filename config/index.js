d219ff3a981f7e2f6ec9d3a33f5126c541ea2b52
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-05 14:23:09 +0200

    connection

---
+++ config/index.js
@@ -0,0 +1,20 @@
+// Import mysql2 and newdb connection
+import { createPool } from "mysql2"; //mysql2 is 
+import 'dotenv/config'
+
+let connection = createPool({
+  host: process.env.hostDb,
+  user: process.env.userDb,
+  password: process.env.pwdDb,
+  database: process.env.dbName,
+  multipleStatements: true,  //allows multiple data to be inserted at the same time
+  connectionLimit: 30 // default is 5 
+})
+connection.on('connection', (err) => { // make use of on to ensure dbconnection is fine. on can be thought of as an event handler and connection is the event
+    if(err) throw new Error('Couldn\'t connect to the database.Please try again later')
+}) 
+
+//throw new to 
+export {
+    connection
+}
