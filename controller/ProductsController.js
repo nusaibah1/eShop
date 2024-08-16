8c91a686a638cc8567fc9c8b68ab6e5a21828ea5
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-16 09:53:04 +0200

    productControllerAdjustment

--- controller/ProductsController.js
+++ controller/ProductsController.js
@@ -1,35 +1,45 @@
 import express from 'express'
 import bodyParser, { json } from 'body-parser'
 import { products } from '../model/index.js'
+import { verifyAToken } from '../middleware/AuthenticateUser.js'
+
 
 
 const productRouter = express.Router()
+productRouter.use(bodyParser.json())
 
-productRouter.use(bodyParser, json())
-
-productRouter.get('/products', (req, res) => {
-    products.fetchProducts(req, res)
+// Retrieve all Products
+productRouter.get('/', verifyAToken , (req, res) => {
+products.fetchProducts(req, res)
 })
 
-productRouter.get('/product/:id', (req, res) => {
+// Recent Products
+productRouter.get('/recent', (req, res) => {
+    products.recentProducts(req, res)
+})
+
+//Rerieve Single Product
+productRouter.get('/:id', verifyAToken, (req, res) => {
     products.fetchProduct(req, res)
 })
 
-productRouter.post('/add', (req, res) => {
-    products.addProduct(req,res)
+// Add product
+productRouter.post('/add', verifyAToken, (req, res) => {
+    products.addProduct(req, res)
 })
 
-productRouter.patch('/product/:id', (req, res) => {
+//Update product
+productRouter.patch('/:id', verifyAToken, (req, res) => {
     products.updateProduct(req, res)
 })
 
-productRouter.delete('/product/:id', (req, res) => {
+//Delete Product
+productRouter.delete('/:id', verifyAToken, (req, res) => {
     products.deleteProduct(req, res)
 })
 
-
-
 export {
-    express,
     productRouter
 }
+
+
