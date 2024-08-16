69539747f378ffbd997d20377f065f73db705ab1
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-16 15:51:06 +0200

    navBar&Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <NavBar />
+    <main >
+      <router-view/>
+    </main>
+    <Footer />
+  </div>
+   
+
 </template>
 
 <style src="@/assets/css/stylesheet.css">
 
 </style>
+<script>
+import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+  components: {
+NavBar,
+Footer
+  }
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <router-link
+     class="navbar-brand" href="#">
+     <img src="https://nusaibah01.github.io/all-images/images/Mylogo.png.png" loading="lazy" alt="logo" class="img-fluid"></router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+
+    export default {
+
+    
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
