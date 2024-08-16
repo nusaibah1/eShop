2db180bf74d85026f289ed40a845f17daac2ec39
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-16 14:41:55 +0200

    Card Slot

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="card">
+<div class="card-header">
+<slot name="cardHeader"></slot>
+</div>
+  <div class="card-body">
+
+   
+  <slot name="cardBody"></slot>
+  </div>
+</div>
+</template>
+
+<script>
+
+    export default {
+components: {
+name: "CardComp"
+
+}
+    
+
+}
+</script>
+
+<style scoped>
+
+.card{
+ width: 18rem;
+}
+</style>
