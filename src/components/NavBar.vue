771af49d88d86aa74042454e4b9e34be98fce8eb
nusaibah1 <charifnusaibah01@gmail.com>
2024-08-16 16:04:01 +0200

    href2to

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
  <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <router-link
-     class="navbar-brand" href="#">
+     class="navbar-brand" to="/">
      <img src="https://nusaibah01.github.io/all-images/images/Mylogo.png.png" loading="lazy" alt="logo" class="img-fluid"></router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
